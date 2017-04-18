d25f591e65b9425279c311a121d14c7fbc8f4599
Thiago Holanda <unnamedd@gmail.com>
2017-04-18 04:06:05 -0300

    added controller Share to use later

---
+++ Sources/App/Controllers/ShareController.swift
@@ -0,0 +1,14 @@
+import Vapor
+import HTTP
+//
+//final class ShareController: ResourceRepresentable {
+//    func proposal(_ req: Request) throws -> ResponseRepresentable {
+//        print("Request: \(req.data)")
+//        
+//        return try JSON(node: ["title": "My Title", "description": "My Description"])
+//    }
+//    
+//    func makeResource() -> Resource<Proposal> {
+//        
+//    }
+//}
