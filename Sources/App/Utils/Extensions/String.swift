430d13e64c22acb946235e2ee748abcf7cae4921
Thiago Holanda <unnamedd@gmail.com>
2017-05-20 05:26:02 -0300

    String extension to add .trim method

---
+++ Sources/App/Utils/Extensions/String.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension String {
+    
+    /**
+     Trim left and right string to white spaces and new lines
+     
+     - returns: Trimmed string
+     */
+    func trim() -> String {
+        return self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
