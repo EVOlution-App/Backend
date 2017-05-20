f6aba9728421878b68c1223702ba730bdd3b2f49
Thiago Holanda <unnamedd@gmail.com>
2017-05-20 05:26:51 -0300

    #1 changed server configuration from Controller to Config.Server

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,7 +1,3 @@
-/**
-
-**/
-
 import Foundation
 import Kitura
 import LoggerAPI
@@ -10,8 +6,11 @@ import Controller
 
 // HeliumLogger disables all buffering on stdout
 HeliumLogger.use(LoggerMessageType.info)
+
 let controller = Controller()
-Log.info("Server will be started on '\(controller.url)'.")
-Kitura.addHTTPServer(onPort: controller.port, with: controller.router)
+Log.info("Server will be started on '\(Config.Server.shared.url)'.")
+
+Kitura.addHTTPServer(onPort: Config.Server.shared.port, with: controller.router)
+
 // Start server
 Kitura.run()
