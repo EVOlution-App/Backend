078ee995d5566aa8e90025121738e7c6753888dd
Thiago Holanda <unnamedd@gmail.com>
2017-05-21 05:22:38 -0300

    added new structs to Config.swift

--- Sources/App/Utils/Config.swift
+++ Sources/App/Utils/Config.swift
@@ -20,13 +20,35 @@ struct Config {
     }
     
     struct Common {
-        struct Regex {
-            static var proposalID: String {
-                return "SE-([0-9]+)"
+        struct Proposal {
+            struct Regex {
+                static var proposalID: String {
+                    return "SE-([0-9]+)"
+                }
+                
+                static var bugID: String {
+                    return "SR-([0-9]+)"
+                }
             }
             
-            static var bugID: String {
-                return "SR-([0-9]+)"
+            struct URLBase {
+                static func markdown(_ link: String) -> String {
+                    return "https://raw.githubusercontent.com/apple/swift-evolution/master/proposals/\(link)"
+                }
+                
+                static func proposal(_ link: String) -> String {
+                    return "https://github.com/apple/swift-evolution/blob/master/proposals/\(link)"
+                }
+            }
+            
+            struct Deeplink {
+                static func proposal(_ id: String) -> String {
+                    return "evo://proposal/\(id)"
+                }
+                
+                static func profile(_ username: String) -> String {
+                    return "evo://username/\(username)"
+                }
             }
         }
     }
