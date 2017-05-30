c4007de8a218a8ee0718133892041b8f5eb8b570
Thiago Holanda <unnamedd@gmail.com>
2017-05-30 01:29:27 -0300

    removed closures from endpoint closures on Controller.swift

--- Sources/App/Controller/Controller.swift
+++ Sources/App/Controller/Controller.swift
@@ -22,49 +22,11 @@ public class Controller {
         // Serve static content from "public"
         router.all("/", middleware: StaticFileServer(path: Config.Server.Resources.staticFiles))
         
-        
         // MARK: /
-        router.get("/") { (request, response, next) in
-            Log.debug("GET - / route handler...")
-            defer {
-                next()
-            }
-
-            try response.render(Config.Common.Templates.landing.rawValue,
-                                context: [:]).end()
-        }
+        router.get("/", handler: LandingController.index)
         
         // MARK: /share/proposal/:proposal
-        router.get("/share/proposal/:proposal") { (request, response, next) in
-            Log.debug("GET - /share/proposal/:proposal route handler...")
-            defer {
-                next()
-            }
-            
-            guard
-                let proposalID = request.parameters["proposal"]
-                else {
-                    try response.status(.badRequest).end()
-                    return
-            }
-            
-            guard
-                let proposals = Service.getProposals()
-                else {
-                    try response.status(.internalServerError).end()
-                    return
-            }
-            
-            guard
-                let proposal = proposals.find(id: proposalID)
-                else {
-                    try response.status(.notFound).end()
-                    return
-            }
-            
-            try response.render(Config.Common.Templates.shareProposal.rawValue,
-                                context: proposal.serialize())
-        }
+        router.get("/share/proposal/:proposal", handler: ProposalsController.get)
     }
 }
 
