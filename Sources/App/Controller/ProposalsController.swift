3733607dd5f2248f2a3b1c9e22850de63ecb2ddc
Thiago Holanda <unnamedd@gmail.com>
2017-05-30 01:28:52 -0300

    added endpoints list and get into ProposalsController.swift

---
+++ Sources/App/Controller/ProposalsController.swift
@@ -0,0 +1,46 @@
+import Foundation
+import Kitura
+import LoggerAPI
+import KituraStencil
+
+final class ProposalsController {
+    public static func list(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        
+    }
+    
+    public static func get(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        Log.debug("GET - /share/proposal/:proposal route handler...")
+        defer {
+            next()
+        }
+        
+        guard
+            let proposalID = request.parameters["proposal"]
+            else {
+                try response.status(.badRequest).end()
+                return
+        }
+        
+        Service.getProposals { [unowned response, next] (error, proposals) in
+            guard
+                let proposals = proposals,
+                error == nil
+                else {
+                    try? response.status(.internalServerError).end()
+                    return
+            }
+            
+            guard
+                let proposal = proposals.find(id: proposalID)
+                else {
+                    try? response.status(.notFound).end()
+                    return
+            }
+            
+            _ = try? response.render(Config.Common.Templates.shareProposal.rawValue,
+                                     context: proposal.serialize())
+            next()
+        }
+    }
+    
+}
