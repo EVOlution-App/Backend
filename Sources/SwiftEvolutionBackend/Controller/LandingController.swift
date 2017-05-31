5dca6b80b195506c92eb107ffbc7f0906be10e02
Taylor Franklin <tfranklin@us.ibm.com>
2017-05-31 16:02:19 -0500

    minor change

--- Sources/SwiftEvolutionBackend/Controller/LandingController.swift
+++ Sources/SwiftEvolutionBackend/Controller/LandingController.swift
@@ -6,11 +6,9 @@ import KituraStencil
 final class LandingController {
     public static func index(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
         Log.debug("GET - / route handler...")
-        defer {
-            next()
-        }
-        
+
         try response.render(Config.Common.Templates.landing.rawValue,
                             context: [:]).end()
+        next()
     }
 }
