1db3d9c0836433676aabfe1c64de0a74eea7af31
Taylor Franklin <tfranklin@us.ibm.com>
2017-06-02 15:59:38 -0500

    added enum for key references

--- Sources/SwiftEvolutionBackend/Utils/Config.swift
+++ Sources/SwiftEvolutionBackend/Utils/Config.swift
@@ -5,6 +5,21 @@ import Configuration
  Constant settings to use throughout the application
  */
 public struct Config {
+    
+    /// Enum containing all json path keys for properties file
+    private enum Keys: String {
+        case resourceTemplates = "serverResources:templates"
+        case staticResourceFiles = "serverResources:staticFiles"
+        case templateLanding = "templates:landing"
+        case templateShareProposal = "templates:shareProposal"
+        case rawProposalsBaseURL = "common:urlBase:proposals"
+        case markdownBaseURL = "urlBase:markdown"
+        case proposalBaseURL = "urlBase:proposal"
+        case proposalDeepLink = "deepLink:proposal"
+        case profileDeepLink = "deepLink:profile"
+        case proposalRegex = "regex:proposalID"
+        case bugRegex = "regex:bugID"
+    }
 
     private let configuration: ConfigurationManager
     private let propertiesFile = "properties.json"
@@ -29,47 +44,47 @@ public struct Config {
     // MARK: Templating Values
     
     var resourceTemplates: String {
-        return configuration["serverResources:templates"] as? String ?? "./Resources/Templates/"
+        return configuration[Keys.resourceTemplates.rawValue] as? String ?? "./Resources/Templates/"
     }
     var staticResourceFiles: String {
-        return configuration["serverResources:staticFiles"] as? String ?? "./Resources/public/"
+        return configuration[Keys.staticResourceFiles.rawValue] as? String ?? "./Resources/public/"
     }
     var templateLanding: String {
-        return configuration["templates:landing"] as? String ?? "index.stencil"
+        return configuration[Keys.templateLanding.rawValue] as? String ?? "index.stencil"
     }
     var templateShareProposal: String {
-        return configuration["templates:shareProposal"] as? String ?? "share_index.stencil"
+        return configuration[Keys.templateShareProposal.rawValue] as? String ?? "share_index.stencil"
     }
     
     // MARK: URL and Link Reference Values
     
     var rawProposalsBaseURL: String {
-        return configuration["common:urlBase:proposals"] as? String ?? "https://data.swift.org/swift-evolution/proposals"
+        return configuration[Keys.rawProposalsBaseURL.rawValue] as? String ?? "https://data.swift.org/swift-evolution/proposals"
     }
     func markdownBaseURL(_ link: String) -> String {
-        let base = configuration["urlBase:markdown"] as? String ?? "https://raw.githubusercontent.com/apple/swift-evolution/master/proposals/"
+        let base = configuration[Keys.markdownBaseURL.rawValue] as? String ?? "https://raw.githubusercontent.com/apple/swift-evolution/master/proposals/"
         return base + link
     }
     func proposalBaseURL(_ link: String) -> String {
-        let base = configuration["urlBase:proposal"] as? String ?? "https://github.com/apple/swift-evolution/blob/master/proposals/"
+        let base = configuration[Keys.proposalBaseURL.rawValue] as? String ?? "https://github.com/apple/swift-evolution/blob/master/proposals/"
         return base + link
     }
     func proposalDeepLink(_ id: String) -> String {
-        let base = configuration["deepLink:proposal"] as? String ?? "evo://proposal/"
+        let base = configuration[Keys.proposalDeepLink.rawValue] as? String ?? "evo://proposal/"
         return base + id
     }
-    func profileDeepLInk(_ username: String) -> String {
-        let base = configuration["deepLink:profile"] as? String ?? "evo://username/"
+    func profileDeepLink(_ username: String) -> String {
+        let base = configuration[Keys.profileDeepLink.rawValue] as? String ?? "evo://username/"
         return base + username
     }
     
     // MARK: Common Utility Values
     
     var proposalRegex: String {
-        return configuration["regex:proposalID"] as? String ?? "SE-([0-9]+)"
+        return configuration[Keys.proposalRegex.rawValue] as? String ?? "SE-([0-9]+)"
     }
     var bugRegex: String {
-        return configuration["regex:bugID"] as? String ?? "SR-([0-9]+)"
+        return configuration[Keys.bugRegex.rawValue] as? String ?? "SR-([0-9]+)"
     }
     
 }
